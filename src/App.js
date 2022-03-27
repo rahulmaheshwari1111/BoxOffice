5ecb772e737091a2791229bd3a869e01bc996e06
rahulmaheshwari1111 <rahulmaheshwari240@gmail.com>
2022-03-28 02:43:05 +0530

    updated meme generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
+  const [meme, setMeme] = useState("http://i.imgflip.com/1bij.jpg")
+  const randomMeme =()=>{
+   fetch('https://api.imgflip.com/get_memes').then(res=>res.json()).then(content=>
+  
+   setMeme(content.data.memes[Math.floor(Math.random()*100)].url)
+   )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+      <h1>😀😀👻🐱‍🏍 Welcome to MEME Generator 😀😀👻🐱‍🏍</h1>
+      <h2> 😎😎 Let Your Sarcasm ROLLL!!😎😋</h2>
+     
+     <div style ={{marginTop:"2em"}}>
+      <input
+        placeholder="Edit on Top.."
+        value={topText}
+        onChange={(e) => setTopText(e.target.value)}
+      />
+      <button  className="cutButton" onClick ={()=>setTopText('')}>✂</button>
+      <input placeholder="Edit on Bottom.." value={bottomText} onChange={(e) => setBottomText(e.target.value)} />
+      <button  className="cutButton" onClick ={()=>setBottomText('')}>✂</button>
+
+      <br/>
+     
+      <button  className = "memeButton" onClick ={()=>randomMeme()}> Generate MEME</button>
+      <br/>
+      </div>
+      <div className="meme">
+      <img src= {meme} alt ="meme" width = "550" height ="550" />
+      <h2 className ="topText">{topText} </h2>
+      <h2 className ="bottomText">{bottomText} </h2>
+      </div>
+      </center>
     </div>
   );
 }
